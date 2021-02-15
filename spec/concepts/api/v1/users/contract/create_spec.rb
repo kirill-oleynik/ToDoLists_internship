c53f4e7a7d75b1bbdec5a3d9c5fddb14b7800093
Kirill Oleynik <kirill.olejnik@gmail.com>
2021-02-15 17:10:57 +0200

    Fix linter issues at spec files

--- spec/concepts/api/v1/users/contract/create_spec.rb
+++ spec/concepts/api/v1/users/contract/create_spec.rb
@@ -34,37 +34,43 @@ RSpec.describe API::V1::Users::Contract::Create, type: :contract do
       end
     end
   end
+
   context 'when one attribute is invalid' do
     describe ':username' do
-    end
-describe ':email' do
-  let(:params) { attributes_for(:user).merge( username: 0 ) }
-  it 'returns expected error' do
+      let(:params) { attributes_for(:user).merge(username: 0) }
+
+      it 'returns expected error' do
         expect(contract.errors.size).to eq(1)
         expect(contract.errors[:username]).to include('must be a string')
-  end
-end
-describe ':email format' do
-  let(:params) { attributes_for(:user).merge( email: 'email' ) }
-  it 'returns expeted error' do
+      end
+    end
+
+    describe ':email format' do
+      let(:params) { attributes_for(:user).merge(email: 'email') }
+
+      it 'returns expeted error' do
         expect(contract.errors.size).to eq(1)
         expect(contract.errors[:email]).to include('is in invalid format')
+      end
+
+      describe ':email data type' do
+        let(:params) { attributes_for(:user).merge(email: 0) }
+
+        it 'returns expected error' do
+          expect(contract.errors.size).to eq(1)
+          expect(contract.errors[:email]).to include('must be a string')
+        end
+      end
+    end
   end
-  describe ':email data type' do
-  let(:params) { attributes_for(:user).merge( email: 0 ) }
-  it 'returns expected error' do
-        expect(contract.errors.size).to eq(1)
-        expect(contract.errors[:email]).to include('must be a string')
-  end
-  end
-end
-  end
+
   context 'when all attributes are invalid' do
     let(:params) { { username: 0, email: 'email' } }
+
     it 'returns expected errors' do
-        expect(contract.errors.size).to eq(2)
-        expect(contract.errors[:email]).to include('is in invalid format')
-        expect(contract.errors[:username]).to include('must be a string')
+      expect(contract.errors.size).to eq(2)
+      expect(contract.errors[:email]).to include('is in invalid format')
+      expect(contract.errors[:username]).to include('must be a string')
     end
   end
 end
