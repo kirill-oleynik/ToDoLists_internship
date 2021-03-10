11fe4b82ff6dc001d05cc24b59e24ba9d97a7311
Kirill Oleynik <kirill.olejnik@gmail.com>
2021-03-10 15:51:29 +0200

    Reafactor GraphQL initializing (Previous PR)

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class APIController < ApplicationController
+  def execute
+    variables = prepare_variables(params[:variables])
+    query, operation_name = params.values_at(:query, operationName)
+    result = ToDoListsInternshipSchema.execute(query, variables: variables, context: {},
+                                                      operation_name: operation_name)
+    render json: result
+  end
+
+  private
+
+  def prepare_variables(variables_param)
+    return {} unless variables_param
+
+    case variables_param
+    when String
+      variables_param.present? ? variables_param(JSON.parse(variables_param)) : {}
+    when Hash, ActionController::Parameters
+      variables_param
+    else
+      raise ArgumentError, "Unexpected parameter: #{variables_param}"
+    end
+  end
+end
