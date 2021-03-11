9d0251f2f4e501ad9f340d44005e0a3f0180a4a6
Kirill Oleynik <kirill.olejnik@gmail.com>
2021-03-11 12:10:34 +0200

    Move schema settings constants to constants file

--- app/graphql/to_do_lists_internship_schema.rb
+++ app/graphql/to_do_lists_internship_schema.rb
@@ -2,12 +2,8 @@
 
 # Application GraphQL Schema
 class ToDoListsInternshipSchema < GraphQL::Schema
-  SHEMA_SETTINGS = {
-    MAX_COMPLEXITY: 100,
-    MAX_DEPTH: 10
-  }.freeze
   mutation(Types::MutationType)
   query(Types::QueryType)
-  max_complexity(SHEMA_SETTINGS[:MAX_COMPLEXITY])
-  max_depth(SHEMA_SETTINGS[:MAX_DEPTH])
+  max_complexity(Constants::GraphQL::SHEMA_SETTINGS[:MAX_COMPLEXITY])
+  max_depth(Constants::GraphQL::SHEMA_SETTINGS[:MAX_DEPTH])
 end
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,4 +3,10 @@ module Constants
     PASSWORD_MIN_SIZE = 6
     EMAIL_REGEXP = /.+@.+\..+/.freeze
   end
+  module GraphQL
+    SHEMA_SETTINGS = {
+    MAX_COMPLEXITY: 100,
+    MAX_DEPTH: 10
+  }.freeze
+  end
 end
