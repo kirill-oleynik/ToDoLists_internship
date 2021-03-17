fa3ce565042431d556b7017d489b617b3d358f4d
Kirill Oleynik <kirill.olejnik@gmail.com>
2021-03-17 13:46:24 +0200

    Add Base resolver

---
+++ app/graphql/resolvers/base.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Resolvers
+  # Base resolver
+  class Base < GraphQL::Schema::Resolver
+    def current_user
+      context[:current_user]
+    end
+
+    def match_operation(operation_result)
+      MatchOperationResult.new.call(
+        operation_result: operation_result,
+        context: context
+      )
+    end
+  end
+end
