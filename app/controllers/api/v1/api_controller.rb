c270d07a1d3e4ca8797e91da7333db9f189b31e2
Kirill Oleynik <kirill.olejnik@gmail.com>
2021-04-07 13:22:21 +0300

    Add Simplendpoint behaviour to APIController

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,6 +3,26 @@
 module API
   module V1
     class APIController < ApplicationController
+      include SimpleEndpoint::Controller
+
+      private
+
+      def default_cases
+        {
+          success: ->(result) { result.success? },
+          invalid: ->(result) { result.failure? }
+        }
+      end
+
+      def default_handler
+        {
+          success: ->(result, **opts) { render json: result['model'], **opts, status: 200 },
+          invalid: lambda { |result, **|
+                     render json: result['contract.default'].errors, serializer: ErrorSerializer,
+                            status: :unprocessable_entity
+                   }
+        }
+      end
     end
   end
 end
