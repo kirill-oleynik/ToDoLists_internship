412c1822eb0a97f0e01a95680cf670b6ebc9b5ca
Kirill Oleynik <kirill.olejnik@gmail.com>
2021-04-14 13:38:22 +0300

    Define sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post 'session', to: 'sessions#create', as: 'create_session'
+  put 'session', to: 'sessions#update', as: 'update_session'
+  delete 'session', to: 'sessions#destroy', as: 'delete_session'
   post "/graphql", to: "graphql#execute"
     namespace :api, path: '/', constaints: { subdomain: 'api' } do
       namespace :v1 do
