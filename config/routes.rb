d8704773ab7d2b3273e578250654ccf0a8caa998
Kirill Oleynik <kirill.olejnik@gmail.com>
2021-04-15 12:31:42 +0300

    Add Authentication routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  post 'session', to: 'sessions#create', as: 'create_session'
-  put 'session', to: 'sessions#update', as: 'update_session'
-  delete 'session', to: 'sessions#destroy', as: 'delete_session'
   post "/graphql", to: "graphql#execute"
+
     namespace :api, path: '/', constaints: { subdomain: 'api' } do
       namespace :v1 do
-  resources :users, except: :index
+        post 'sgn_up', to: 'registrations#create', as: 'sign_up'
+        post 'sign_in', to: 'sessions#create', as: 'sign_in'
+        put 'refresh_session', to: 'sessions#update', as: 'update_session'
+        delete 'sign_out', to: 'sessions#destroy', as: 'sign_out'
       end
     end
 end
