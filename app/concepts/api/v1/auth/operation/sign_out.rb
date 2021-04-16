a34d5e34811c5cbf8a92c5d89d7f13f3e20acbad
Kirill Oleynik <kirill.olejnik@gmail.com>
2021-04-16 14:35:09 +0300

    Refacotr Sign Out operation Rescue the error only at the step that can cause it

--- app/concepts/api/v1/auth/operation/sign_out.rb
+++ app/concepts/api/v1/auth/operation/sign_out.rb
@@ -3,9 +3,9 @@
 module API::V1::Auth::Operation
   # SignUp User operation
   class SignOut < ApplicationOperation
+    step Contract::Build(constant: API::V1::Auth::Contract::RefreshToken)
+    step Contract::Validate()
     step Rescue(JWTSessions::Errors::Unauthorized, handler: :set_error_info) {
-      step Contract::Build(constant: API::V1::Auth::Contract::RefreshToken)
-      step Contract::Validate()
       step :sign_out
     }
 
