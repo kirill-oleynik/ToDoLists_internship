2d844540e110979f9dc1126844fd0ffffaf75fe4
Kirill Oleynik <kirill.olejnik@gmail.com>
2021-04-16 14:08:25 +0300

    Refactor Refresh Session operation Catch the error only at the step that can cause it

--- app/concepts/api/v1/auth/operation/refresh_session.rb
+++ app/concepts/api/v1/auth/operation/refresh_session.rb
@@ -3,9 +3,9 @@
 module API::V1::Auth::Operation
   # SignUp User operation
   class RefreshSession < ApplicationOperation
+    step Contract::Build(constant: API::V1::Auth::Contract::RefreshToken)
+    step Contract::Validate()
     step Rescue(JWTSessions::Errors::Unauthorized, handler: :render_status) {
-      step Contract::Build(constant: API::V1::Auth::Contract::RefreshToken)
-      step Contract::Validate()
       step :refresh_session
     }
 
