0ab4c3c710f1516877466132c6826528b453b765
Kirill Oleynik <kirill.olejnik@gmail.com>
2021-04-22 12:34:40 +0300

    update refresh session operatio class description

--- app/concepts/api/v1/auth/operation/refresh_session.rb
+++ app/concepts/api/v1/auth/operation/refresh_session.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module API::V1::Auth::Operation
-  # SignUp User operation
+  # refresh session operation
   class RefreshSession < ApplicationOperation
     step :call_contract
     step Rescue(JWTSessions::Errors::Unauthorized, handler: :render_status) {
