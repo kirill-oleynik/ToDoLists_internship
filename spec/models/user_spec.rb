85d1b3ff0726832f53aa329bee470eaa23312baa
Kirill Oleynik <kirill.olejnik@gmail.com>
2021-05-12 12:27:28 +0300

    Refactor specs use built-in RSpec calidation methods for model dependencies

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,6 @@
 
 RSpec.describe User, type: :model do
   describe 'associations' do
-    it { is_expected.to have_many(:tasks) }
-
-    it 'destroys dependent tasks after deletion' do
-      expect(Task.count).to equal(0)
-      user = create_user_with_tasks(tasks_count: 10)
-      expect(Task.count).to equal(10)
-      user.destroy
-      expect(Task.count).to equal(0)
-    end
+    it { is_expected.to have_many(:tasks).dependent(:destroy) }
   end
 end
